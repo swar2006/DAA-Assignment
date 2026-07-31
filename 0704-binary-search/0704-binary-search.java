5c7728cc4fa4a2460e2a66a78ab54990d86796af
PARIKH SWAR KEYURKUMAR <138838107+swar2006@users.noreply.github.com>
2026-07-31 10:53:01 +0530

    Time: 0 ms (100%), Space: 48.6 MB (14.31%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        return binarySearch(nums, target, 0, nums.length - 1);
+    }
+    public int binarySearch(int[] nums, int target, int left, int right)
+    {
+        if(left > right)
+        {
+            return -1;
+        }
+        int mid = left + (right - left) / 2;
+
+        if(nums[mid] == target)
+        {
+            return mid;
+        }
+        if(nums[mid] < target)
+        {
+            return binarySearch(nums, target, mid + 1, right);
+        }
+        return binarySearch(nums, target, left, mid - 1);
+    }
+}
